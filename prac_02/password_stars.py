8079048347d9b5010d5a327cdf46eb4d0c23c5a0
Scott_Dyer <scott.dyer@my.jcu.edu.au>
2024-09-26 14:43:11 +1000

    Add docstrings to password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,24 @@
+"""
+CP1404 - Practical 02
+write a program that asks the user for a password, with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
+"""
+
 MINIMUM_LENGTH = 6
 
 
 def main():
+    """Program to get valid password and print asterisks."""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print asterisks equal to the length of password."""
     print("*" * len(password))
 
 
 def get_password():
+    """Get valid password length greater than minimum."""
     password = input("Enter a password: ")
     while len(password) < MINIMUM_LENGTH:
         print(f"Please enter a password of length {MINIMUM_LENGTH} or greater")
