3c379c4deaa11d258c9dae664d4c9a0e3f3a64d9
Scott_Dyer <scott.dyer@my.jcu.edu.au>
2024-09-26 14:23:38 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,21 @@
 PASSWORD_LENGTH = 6
 
-password = input("Enter a password: ")
-while len(password) < PASSWORD_LENGTH:
-    print("Please enter a password of length 6 or greater")
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter a password: ")
-print("*" * len(password))
+    while len(password) < PASSWORD_LENGTH:
+        print("Please enter a password of length 6 or greater")
+        password = input("Enter a password: ")
+    return password
+
+
+main()
