9c90fdaf122bb571502f6617a0805639e5cdc48c
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-09-27 15:33:39 +1000

    Add get valid score function to score_menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,14 +4,15 @@ Score menu program
 """
 
 MENU = "(G)et a valid score \n(P)rint result \n(S)how stars \n(Q)uit"
-
+MINIMUM = 0
+MAXIMUM = 100
 
 def main():
     print(MENU)
     choice = input("Please select an option above: ").upper()
     while choice != "Q":
         if choice == "G":
-            pass
+            score = get_valid_score()
         elif choice == "P":
             pass
         elif choice == "S":
@@ -20,6 +21,14 @@ def main():
             print("Invalid choice.")
             print(MENU)
             choice = input("Please choose an option above: ").upper()
+
+
+def get_valid_score():
+    valid_score = int(input("Please enter your score: "))
+    while valid_score < MINIMUM or valid_score > MAXIMUM:
+        print("Invalid score")
+        valid_score = int(input("Please enter your score: "))
+    return valid_score
 
 
 main()
