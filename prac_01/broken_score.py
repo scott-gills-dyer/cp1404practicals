e487cb38d78089a007117b6d02a68572c9278855
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-09-27 12:51:27 +1000

    Refactor score decision structure in broken_score

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -6,10 +6,9 @@ Program to determine score status
 score = float(input("Enter score: "))
 if score < 0 or score > 100:
     print("Invalid score")
+elif score < 50:
+    print("Bad")
+elif score < 90:
+    print("Passable")
 else:
-    if score < 50:
-        print("Bad")
-    elif score < 90:
-        print("Passable")
-    else:
-        print("Excellent")
+    print("Excellent")
