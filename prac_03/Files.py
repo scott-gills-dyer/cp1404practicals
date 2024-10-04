d0c9aaa6bdc731df64ec1cc377f73c70ad9f20b8
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-10-05 01:05:01 +1000

    Add read from file to files program

--- prac_03/Files.py
+++ prac_03/Files.py
@@ -3,9 +3,20 @@ CP1404/CP5632 - Practical
 Create a new file called files.py and do all the following separate questions in it.
 """
 
-# Question 1.
+# Question 1
 
 out_file = open("names.txt", "w")
 name = input("What is your name: ")
 print(name, file=out_file)
 out_file.close()
+
+# Question 2
+
+in_file = open("names.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hello {name}")
+
+
+
+
