1b19d13e1898ad4360456c80d59b2fd2bf924196
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-10-17 06:45:40 +1000

    Refactor if structure to try-except and add loop through CODE_TO_NAME with formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,19 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
 state = input("Enter short state: ").upper()
 while state != "":
-    if state in CODE_TO_NAME:
+    try:
         print(state, "is", CODE_TO_NAME[state])
-    else:
+    except KeyError:
         print("Invalid short state")
+
     state = input("Enter short state: ").upper()
+
+max_length = max(len(state) for state in (list(CODE_TO_NAME.keys())))
+for state in CODE_TO_NAME:
+    print(f"{state:{max_length}} is {CODE_TO_NAME[state]}")
