f61a9bd2f6987237b651abab2b99b02b83e48d81
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-10-18 15:20:16 +1000

    Finish champions program in wimbledon file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,3 +2,50 @@
 CP1404
 Wimbledon - Game, Set, Match
 """
+
+FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
+
+
+def main():
+    """Program to get record form file, process and print those records."""
+    records = get_records(FILENAME)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
+
+
+def display_results(champion_to_count, countries):
+    """Display champions and countries."""
+    print("Wimbledon Champions")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in sorted(countries)))
+
+
+def process_records(records):
+    """Process records into dictionary and set list of countries."""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
+
+
+def get_records(filename):
+    """Get records form file."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+        return records
+
+
+main()
