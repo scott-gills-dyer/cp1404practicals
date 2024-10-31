61476280cbb4083c17555ee401ce0d561d624eaf
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-01 09:49:45 +1000

    Add docstrings and remove print for python language to languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,12 +3,13 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
+    """Program to print python class and print dynamic languages."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
     languages = [python, ruby, visual_basic]
-    print(python)
+    print(f"The dynamically typed languages are:")
     for language in languages:
         if language.is_dynamic():
             print(language.name)
