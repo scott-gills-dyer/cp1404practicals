ccb2e3d6ec4f4ac71681bc2ca581263ae97c84d2
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-01 14:07:37 +1000

    Add guitar class and methods, get_age, is_vintage and, __str__

---
+++ prac_06/guitar.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+
+"""
+
+CURRENT_YEAR = 2024
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """"""
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def get_age(self):
+        """"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """"""
+        return self.get_age() >= VINTAGE_AGE
+
+    def __str__(self):
+        """"""
+        return f"{self.name}, {self.year}, {self.cost}"
