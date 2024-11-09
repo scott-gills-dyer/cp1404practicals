2977e51274ac21d3d0d7d86b47d6c12f0bf7f135
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-10 06:09:22 +1000

    Add date filtering, get input and, update project functions; Add save options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,12 +5,13 @@ Project Management Program
 Estimate: 4 Hours
 Actual:
 """
-
 from project import Project
+from datetime import datetime
 
 FILENAME = "projects.txt"
 MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n"
         "(F)ilter by date\n(A)dd new project\n(U)pdate project")
+
 
 def main():
     projects = []
@@ -18,10 +19,11 @@ def main():
     choice = input("Enter your choice: ").upper()
     while choice != "Q":
         if choice == "L":
-            load_project(projects)
+            load_projects(projects)
             print(f"Loaded {len(projects)} from projects.txt")
         elif choice == "S":
-            pass
+            save_projects(projects)
+            print(f"Saved {len(projects)} to projects.txt")
         elif choice == "D":
             if projects:
                 display_incomplete_projects(projects)
@@ -29,7 +31,10 @@ def main():
             else:
                 print("There are no projects loaded")
         elif choice == "F":
-            pass
+            date = get_date()
+            filter_project_date = filter_projects_by_date(date, projects)
+            display_incomplete_projects(filter_project_date)
+
         elif choice == "A":
             name = input("Enter the project name: ").title()
             start_date = input("Start date (dd/mm/yy): ")
@@ -38,18 +43,61 @@ def main():
             completion_percentage = float(input("Percent complete: "))
             projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
         elif choice == "U":
-            for i, project in enumerate(projects):
-                print(i, project)
+            project_choice = get_project_choice(projects)
+            selected_project = projects[project_choice]
+            print(selected_project)
+            update_project(selected_project)
         else:
             print("Invalid choice")
             print(MENU)
         print(MENU)
         choice = input("Enter your choice: ").upper()
+    save_option = input("Would you like to save to projects.txt? (Y/n): ")
+    if save_option != "n":
+        save_projects(projects)
+        print("projects saved")
+    print("Thank you for using custom-built project management software.")
+
+
+def filter_projects_by_date(date, projects):
+    """"""
+    filter_project_date = [project for project in projects if project.start_date > date]
+    return filter_project_date
+
+
+def get_date():
+    """"""
+    date_string = input("Show projects that start after date (dd/mm/yy): ")
+    date = datetime.strptime(date_string, "%d/%m/%Y").date()
+    return date
+
+
+def update_project(selected_project):
+    """"""
+    new_percentage = float(input("New percentage: "))
+    new_priority = int(input("New priority: "))
+    selected_project.completion_percentage = float(new_percentage)
+    selected_project.priority = int(new_priority)
+
+
+def save_projects(projects):
+    """"""
+    with open(FILENAME, "w", encoding="utf-8-sig") as out_file:
+        for project in projects:
+            out_file.write(f"{project}\n")
+
+
+def get_project_choice(projects):
+    """"""
+    for i, project in enumerate(projects):
+        print(i, project)
+    project_choice = int(input("Project choice: "))
+    return project_choice
 
 
 def display_complete_projects(projects):
     """"""
-    print("completed projects")
+    print("completed projects:")
     for project in projects:
         if project.is_complete():
             print(project)
@@ -57,13 +105,14 @@ def display_complete_projects(projects):
 
 def display_incomplete_projects(projects):
     """"""
-    print("Incomplete projects")
+    print("Incomplete projects:")
     for project in projects:
         if not project.is_complete():
             print(project)
 
 
-def load_project(projects):
+def load_projects(projects):
+    """"""
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
