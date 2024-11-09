3b3a23fc1daa67f45fadc6fb2bc2ff428b1bc32d
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-10 03:18:42 +1000

    Add main menu and MENU constant

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -9,13 +9,34 @@ Actual:
 from project import Project
 
 FILENAME = "projects.txt"
-
+MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n"
+        "(F)ilter by date\n(A)dd new project\n(U)pdate project")
 
 def main():
     projects = []
-    load_project(projects)
-    for project in projects:
-        print(project)
+    print(MENU)
+    choice = input("Enter your choice: ").upper()
+    while choice != "Q":
+        if choice == "L":
+            load_project(projects)
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            for project in projects:
+                print(project)
+        elif choice == "F":
+            pass
+        elif choice == "U":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid choice")
+            print(MENU)
+        choice = input("Enter your choice: ").upper()
 
 
 def load_project(projects):
