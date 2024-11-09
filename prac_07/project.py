3fa61d884e664721f55d2a7edcac2d266129bd7f
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-10 08:04:35 +1000

    Add module docstrings

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,15 +2,15 @@
 CP1404 Practical
 
 Estimate: 4 hours
-Actual:
+Actual: 5 Hours
 """
-
+from datetime import datetime
 
 class Project:
-    """"""
+    """Project class that has name, start_date, priority, cost_estimate, completion_percentage."""
 
-    def __init__(self, name="", start_date="", priority=0, cost_estimate=0, completion_percentage=0.0):
-        """"""
+    def __init__(self, name, start_date, priority=0, cost_estimate=0, completion_percentage=0.0):
+        """Initialises a project instance with name, start_date, priority, cost_estimate, completion_percentage."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -19,10 +19,10 @@ class Project:
 
 
     def __str__(self):
-        """"""
+        """Return string of the project object."""
         return (f"{self.name}, Start: {self.start_date}, Priority: {self.priority}, "
                 f"Estimate: ${self.cost_estimate:.2f}, Completion: %{self.completion_percentage}")
 
     def is_complete(self):
-        """"""
+        """Determines if the project has been completed."""
         return self.completion_percentage == 100
