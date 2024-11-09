dcc1c8234e64a9c6c40bf0f3ff6bfbd324d26aee
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-10 00:29:02 +1000

    Refactor __str__ with __init__ method; Add __lt__ or less than method for sorting guitar list in guitar.py

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,9 +1,7 @@
 """
 CP1404 Practical
 
-Estimate: 5 minutes
-Actual:   10 minutes
-
+guitar
 """
 
 CURRENT_YEAR = 2024
@@ -13,11 +11,19 @@ VINTAGE_AGE = 50
 class Guitar:
     """Guitar class has name, year and cost."""
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         """Initialise a guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return string of the guitar object."""
+        return f"{self.name}, {self.year}, ${self.cost:.2f}"
+
+    def __lt__(self, other):
+        """Return if guitar object is older than other guitar."""
+        return self.year < other.year
 
     def get_age(self):
         """Determine the age of the guitar."""
@@ -26,7 +32,3 @@ class Guitar:
     def is_vintage(self):
         """Determine if the guitar is vintage."""
         return self.get_age() >= VINTAGE_AGE
-
-    def __str__(self):
-        """Return string of the guitar object."""
-        return f"{self.name}, {self.year}, {self.cost}"
