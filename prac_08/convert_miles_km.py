7e1be12688bfc20f19b081fa6466367fde5a490b
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-17 06:27:54 +1000

    Add docstrings; Refactor self from function parameters

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,32 +11,32 @@ MILES_TO_KM_RATIO = 1.60934
 
 
 class MilesConverterApp(App):
-    """"""
+    """Kivy App for converting miles to kilometres."""
     output_km = StringProperty()
 
     def build(self):
-        """"""
+        """Build the Kivy App from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, text):
-        """"""
-        miles = self.convert_to_number(self, text)
+        """Handle calculation (could be button press or other call), output result to label widget."""
+        miles = self.convert_to_number(text)
         self.update_result(miles)
 
     def update_result(self, miles):
-        """"""
+        """Update the result label."""
         self.output_km = str(miles * MILES_TO_KM_RATIO)
 
     def handle_increment(self, text, change):
-        """"""
-        miles = self.convert_to_number(self, text) + change
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        miles = self.convert_to_number(text) + change
         self.root.ids.input_miles.text = str(miles)
 
     @staticmethod
-    def convert_to_number(self, text):
-        """"""
+    def convert_to_number(text):
+        """Convert text to float or 0.0 if invalid."""
         try:
             return float(text)
         except ValueError:
