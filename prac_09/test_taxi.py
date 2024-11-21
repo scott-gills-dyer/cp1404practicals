13735fc4a2a56b9cbb12da29de215e2efc22d0fd
Scott <scott.dyer@my.jcu.edu.au>
2024-11-21 13:37:06 +1000

    Refactor my_taxi to not include price_per_km

--- prac_09/test_taxi.py
+++ prac_09/test_taxi.py
@@ -3,7 +3,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Test Taxi class"""
-    my_taxi = Taxi('prius', 100, 1.23)
+    my_taxi = Taxi('prius', 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
