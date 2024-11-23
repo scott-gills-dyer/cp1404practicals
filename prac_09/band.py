063a5013a8c3a132e33c535ea6a163e300b3c459
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-23 13:44:42 +1000

    Add add and play methods

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,14 +1,25 @@
 """
 
 """
+
+
 class Band:
     """"""
+
     def __init__(self, name):
+        """"""
         self.name = name
         self.musicians = []
 
     def __str__(self):
+        """"""
         musician_info = ''.join([str(musician) for musician in self.musicians])
         return f"Band: {self.name}\n{musician_info}"
 
+    def add(self, musician):
+        """"""
+        self.musicians.append(musician)
 
+    def play(self):
+        """"""
+        return '\n'.join([musician.play() for musician in self.musicians])
