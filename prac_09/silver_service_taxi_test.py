86df75578796f658fe274a4209fda016f1e31546
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-23 13:03:44 +1000

    Add silver_service_taxi_test.py to test SilverServiceTaxi class

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """"""
+    taxi = SilverServiceTaxi('Lux Taxi', 100, 2)
+    taxi.drive(50)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
