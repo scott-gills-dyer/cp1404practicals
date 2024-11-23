874449128b22cb4de20510ce31e0060d7b17ac66
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-23 13:14:59 +1000

    Add other_taxi and assertion tests

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,9 +6,15 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """"""
-    taxi = SilverServiceTaxi('Lux Taxi', 100, 2)
+    taxi = SilverServiceTaxi('Lux Taxi', 100, 1.5)
     taxi.drive(50)
     print(taxi)
     print(taxi.get_fare())
+
+    other_taxi = SilverServiceTaxi('Super Lux', 100, 2)
+    assert other_taxi.drive(18)
+    print(other_taxi.fuel)
+    assert other_taxi.get_fare() == 48.78
+    print(other_taxi.get_fare())
 
 main()
