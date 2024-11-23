79588b817b81ba6a8ebe1ba60c942246a0ebae9c
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-23 12:04:59 +1000

    Add good and bad car to test UnreliableCar

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,9 +2,17 @@ from prac_09.unreliable_car import UnreliableCar
 
 
 def main():
-    """"""
-    my_car = UnreliableCar('prius', 100, 50)
-    my_car.drive(40)
-    print(my_car)
+    """Program to test UnreliableCar class."""
+    good_car = UnreliableCar("Decent", 100, 80)
+    bad_car = UnreliableCar("Poor", 100, 20)
+
+    for i in range(1, 12):
+        print(f"Attempting to drive {i}km:")
+        print(f"{good_car.name:12} drove {good_car.drive(i):2}km")
+        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
+
+    print(good_car)
+    print(bad_car)
+
 
 main()
