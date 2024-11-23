f538b984f5df1c6df4831a25b0af2a44322fec29
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2024-11-23 13:15:42 +1000

    Add get_fare and Refactor __str__ methods

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,15 +1,23 @@
+"""
 
+"""
 from prac_09.taxi import Taxi
+
 
 class SilverServiceTaxi(Taxi):
     """"""
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
+        """"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= self.fanciness
 
     def __str__(self):
-        return f"{self.fanciness} {self.price_per_km}"
+        """"""
+        return f"{super().__str__()} plus flagfall of ${self.price_per_km:.2f}"
 
-
+    def get_fare(self):
+        """"""
+        return self.flagfall + super().get_fare()
